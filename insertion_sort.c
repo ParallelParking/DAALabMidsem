8be9885ba4e37fbc0f9d0ba3c18923b0e8d8bf36
ParallelParking <avinashbinand@gmail.com>
2025-02-24 02:24:23 +0530

    small fix

--- insertion_sort.c
+++ insertion_sort.c
@@ -7,36 +7,15 @@ int swap(int* a, int* b) {
     *b = t;
 }
 
-int partition(int a[], int l, int r) {
-    int i = l;
-    int j = r + 1;
-    int p = a[l];
-    do {
-        do {
-            i += 1;
-        } while (a[i] < p);
-        do {
-            j -= 1;
-        } while (a[j] > p);
-        swap(&a[i], &a[j]);
-    } while (i < j);
-    swap(&a[i], &a[j]);
-    swap(&a[l], &a[j]);
-    return j;
-}
-
-void quicksort(int a[], int l, int r) {
-    if (l >= r) return;
-    int s = partition(a, l, r);
-    quicksort(a, l, s - 1);
-    quicksort(a, s + 1, r);
-}
 
 int main() {
     int a[] = {9,8,7,6,5,4,3,2,1,0};
     int n = 10;
-    quicksort(a, 0, 9);
+    for (int i = 1; i < n; i++) {
+        for (int j = i; j > 0; j--) {
+            if (a[j - 1] > a[j]) swap(&a[j], &a[j - 1]);
+        }
+    }
     for (int i = 0; i < n; i++) printf("%d ", a[i]);
     printf("\n");
-    return 0;
 }
